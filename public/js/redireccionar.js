59b6b1803aad0fd3ec785b343a1df09f995554ed
Johan Steven Loaiza Luna <johan.loaiza@xdc.com.co>
2023-11-01 19:39:13 -0500

    Se agrega el proyecto

---
+++ public/js/redireccionar.js
@@ -0,0 +1,3 @@
+document.getElementById("redireccionar").addEventListener("click", function() {
+    window.location.href = "https://wa.me/message/OOU5ZAO5TJKIH1"; // Reemplaza "URL_DEL_OTRO_SITIO" con la URL a la que deseas redirigir.
+});
